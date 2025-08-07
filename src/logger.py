77b01acb4970fc42b420259cf0c12ed799baea22
Raymond Cen <raymondcen07@gmail.com>
2025-08-06 19:37:51 -0700

    setup logging and exception

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m-5d-5Y_%H_%M_%S')}.log" # naming convention for log file
+logs_path=os.path.join(os.getcwd(), "logs",LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)  # even tho there is a folder/file keep appending to file inside
+
+LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format ="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO, 
+
+)
+
+
+#test logger
+
+# if __name__=="__main__":
+#     logging.info("Logging has started")
