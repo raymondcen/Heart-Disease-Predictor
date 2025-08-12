27dac40d84002d2bdae752376417ccd30241f94b
Raymond Cen <raymondcen07@gmail.com>
2025-08-12 15:11:39 -0700

    data ingestion setup

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m-5d-5Y_%H_%M_%S')}.log" # naming convention for log file
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" # naming convention for log file
 logs_path=os.path.join(os.getcwd(), "logs",LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)  # even tho there is a folder/file keep appending to file inside
 
